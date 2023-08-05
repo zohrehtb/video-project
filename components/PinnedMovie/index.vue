7ad3cee721a2dc8abd82d79cee1cc55bb02a56f4
Zohreh <zohreh.tt@gmail.com>
2023-08-05 11:59:05 +0330

    Add home page files

---
+++ components/PinnedMovie/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <section 
+        v-if="content" 
+        class="pinned-movie flex items-end w-full pb-10 relative"
+        :style="{ backgroundImage: `url(${require('~/assets/images/' + content.img)})` }"
+    >
+        <p
+            v-if="content.description"
+            class="text-[20px] text-white absolute w-full right-0 text-center top-[10%] md:top-[30%] md:right-[10%] md:w-[300px] md:text-right md:text-[16px]"
+        >
+            {{ content.description }}
+        </p>
+
+        <div class="container mx-auto p-[15px] md:p-0">
+            <div class="pinned-movie__screens grid grid-cols-3 md:grid-cols-6">
+                <template v-if="content.screen_shots && content.screen_shots.length">
+                    <div
+                        v-for="(screen, index) in content.screen_shots" 
+                        :key="index" 
+                    >
+                        <img 
+                            :src="require(`~/assets/images/${screen.link}`)" 
+                            alt="movie screen shot"
+                        >
+                        <h6 
+                            v-if="screen.title" 
+                            class="text-[16px] text-white mt-2"
+                        >
+                            {{ screen.title }}
+                        </h6>
+                    </div>
+                </template>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name:'pinnedMovie',
+
+    props:{
+        /**
+         * Content
+         */
+        content:Object,
+    }
+}
+</script>
+
+<style lang="scss" src="./index.scss" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,133 @@
 <template>
-  <Tutorial/>
+    <div>
+        <c-header/>
+        <main>
+            <c-slider :items="mainSliderItems"/>
+            <div class="container mx-auto p-[15px] md:p-0">
+                <c-categories :items="categoriesItems"/>
+
+                <c-movie-slider 
+                    :title="titles.sepcial" 
+                    :items="filterPosts('sepcial')"
+                    description
+                    height="133px"
+                />
+
+                <c-movie-slider 
+                    :title="titles.new" 
+                    :items="filterPosts('new')"
+                    height="133px"
+                />
+
+                <c-movie-slider 
+                    :title="titles.next" 
+                    :items="filterPosts('next')"
+                    height="170px"
+                    cols="4"
+                />
+
+                <c-movie-slider 
+                    :title="titles.your" 
+                    :items="filterPosts('your')"
+                    height="133px"
+                />
+            </div>
+
+            <c-pinned-post :content="pinnedItem" />
+
+            <div class="container mx-auto p-[15px] md:p-0">
+
+                <c-movie-slider 
+                    :title="titles.updated" 
+                    :items="filterPosts('updated')"
+                    height="133px"
+                />
+
+                <c-movie-slider 
+                    :title="titles.seen" 
+                    :items="filterPosts('seen')"
+                    height="133px"
+                />
+
+                <c-movie-slider 
+                    :title="titles.your_favorite" 
+                    :items="filterPosts('sepcial')"
+                    description
+                    height="133px"
+                />
+            </div>
+        </main>
+        <c-footer />
+    </div>
 </template>
 
 <script>
+/** Data */
+import json from '../static/data';
+
+/** Components */
+import CHeader from '../components/Header'
+import CFooter from '../components/Footer'
+import CSlider from '../components/MainSlider'
+import CCategories from '../components/Categories'
+import CPinnedPost from '../components/PinnedMovie'
+import CMovieSlider from '../components/MovieSlider'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  /**
+   * @inheritdoc
+  */
+  data() {
+      return {
+          mainSliderItems: json.data.main_slider,
+          categoriesItems: json.data.categories,
+          pinnedItem: json.data.pinned_movie,
+          moviesItems: json.data.movies,
+          titles:{
+            new: 'تازه‌ها',
+            hot: 'داغ ترین',
+            your: 'به تماشا ادامه دهید',
+            updated: 'سریال‌های به‌روز شده',
+            next: 'بزودی',
+            sepcial: 'عناوین ویژه',
+            seen:'دیده‌ شده‌ها',
+            your_favorite:'چون این سریال‌ها را دیده‌اید'
+          },
+      };
+  },
+
+  components:{
+      CHeader,
+      CSlider,
+      CFooter,
+      CCategories,
+      CPinnedPost,
+      CMovieSlider,
+  },
+
+  methods:{
+    /**
+     * Filter posts by category
+     *
+     * @param {*} slug
+     * @returns {*}
+     */
+    filterPosts(slug) {
+        this.filteredPosts = [];
+        this.moviesItems.forEach((post) => {
+            if (post.category && post.category.length) {
+                post.category.forEach((cat) => {
+                    if (cat.name === slug) {
+                        this.filteredPosts.push(post);
+                    }
+                });
+            }
+        });
+
+        return this.filteredPosts;
+    },
+  }
 }
 </script>
